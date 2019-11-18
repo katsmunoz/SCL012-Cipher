d6764689ff32c50bd3dedb9b47772d846c6d0af9
katsmunoz <katherine.smm@gmail.com>
2019-11-18 15:40:42 -0300

    Cifra MINUS y considera ESPACIOS. Tests LISTOS

--- src/cipher.js
+++ src/cipher.js
@@ -5,15 +5,23 @@ window.cipher = {
    for (let i = 0; i < newTxt.length; i++) {
           let asciiNumber = newTxt.charCodeAt(i);
           console.log(asciiNumber)
+//Cifrado para MAYÚSCULAS
               if ((asciiNumber >= 65) && (asciiNumber <= 90)) {
           let code = ((asciiNumber - 65 + parseInt(offsetNumber)) % 26 + 65);
           console.log(code)
           encodeTxt += String.fromCharCode(code);
-              }
-          }
           console.log(encodeTxt)
-
-          return encodeTxt;
+//Cifrado para MINÚSCULAS
+             } if ((asciiNumber >= 97) && (asciiNumber <= 122)) {
+                let code = ((asciiNumber - 97 + parseInt(offsetNumber)) % 26 + 97);
+                encodeTxt += String.fromCharCode(code);                 
+//Para mantener los ESPACIOS
+              } else if (asciiNumber == 32) {
+              encodeTxt += String.fromCharCode(asciiNumber);
+                
+              }        
+        }
+        return encodeTxt; 
 },
 
 decode: (offsetNumber, newTxt) => {
@@ -26,7 +34,15 @@ decode: (offsetNumber, newTxt) => {
          let code = ((asciiNumber - 90 - parseInt(offsetNumber)) % 26 + 90);
          console.log(code)
          encodeTxt += String.fromCharCode(code);
-             }
+//Cifrado para MINÚSCULAS
+        } if ((asciiNumber >= 97) && (asciiNumber <= 122)) {
+          let code = ((asciiNumber - 122 - parseInt(offsetNumber)) % 26 + 122);
+          encodeTxt += String.fromCharCode(code);
+//Para mantener los espacios    
+             } else if (asciiNumber == 32) {
+              encodeTxt += String.fromCharCode(asciiNumber);
+                
+              }  
          }
          console.log(encodeTxt)
 
